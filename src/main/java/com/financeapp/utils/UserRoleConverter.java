c1bf79d00c119ff13e49f9de65553eb9dfa32ff1
Joseph Oluwapelumi Jegede <150534104+JegedeJoseph@users.noreply.github.com>
2026-05-05 14:05:46 +0100

    Refactor UserRoleConverter to extend EnumConverter
    Update UserRoleConverter to inherit from a generic EnumConverter<UserRole> and call super(UserRole.class) in the constructor. Fix the import statement and add a static factory method createUserRoleConverter() for instantiation. These changes centralize enum conversion logic and provide a clearer creation API.

--- src/main/java/com/financeapp/utils/UserRoleConverter.java
+++ src/main/java/com/financeapp/utils/UserRoleConverter.java
@@ -1,11 +1,15 @@
 package com.financeapp.utils;
 
-import com.financeapp.models.enums.UserRole
+import com.financeapp.models.enums.UserRole;
 
-
-public class UserRoleConverter {
+public class UserRoleConverter extends EnumConverter<UserRole> {
     private UserRoleConverter() {
+        super(UserRole.class);
     }
 
+    public static UserRoleConverter createUserRoleConverter() {
+        return new UserRoleConverter();
+    }
+}
 
 }
